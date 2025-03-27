4d2fc776a73fd207d1ee565ad7fbb1e1309afba7
D-Seagull <dchaika.work@gmail.com>
2025-03-27 21:59:25 +0100

    added local server

--- src/scripts/countContacts.js
+++
@@ -1,8 +0,0 @@
-import { readContacts } from '../utils/readContacts.js';
-
-export const countContacts = async () => {
-  const contactList = await readContacts();
-  return contactList.length;
-};
-
-console.log(await countContacts());
